fbc5b6f7326b9fa15794b5f9012eb3cc50ef68e7
Nikomika <o.a.voytova@gmail.com>
2025-01-20 21:20:29 +0300

    Initial commit

---
+++ src/main/java/org/example/FactorialCalc.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class FactorialCalc {
+
+    public static long factorial(int number) {
+        if (number < 0 || number > 100) {
+            System.out.println("Невозможно вычислить факториал");
+        }
+        if (number == 0 || number == 1) {
+            return 1;
+        } else {
+            long result = 1;
+            for (int i = 2; i <= number; i++) {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
+
